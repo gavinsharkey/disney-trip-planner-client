713516c52fdeb5a22da42b2cc9fb59f53c462fa0
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 13:14:56 -0400

    Add createTrip method to API class Add loadDaysByTrip method to API class Add methods for creating and destroying days to API class

--- js/models/api.js
+++ js/models/api.js
@@ -1,4 +1,19 @@
 class API {
+  static createTrip(tripName) {
+    return fetch('http://localhost:3000/trips', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        trip: {
+          name: tripName
+        }
+      })
+    })
+    .then(resp => resp.json())
+  }
+
   static getTripsData() {
     return fetch('http://localhost:3000/trips')
     .then(resp => resp.json())
@@ -9,5 +24,25 @@ class API {
     return fetch(`http://localhost:3000/trips/${tripId}`)
     .then(resp => resp.json())
     .catch(err => console.log(`ERROR: ${err}`))
+  }
+
+  static loadDaysByTrip(tripId) {
+    return fetch(`http://localhost:3000/trips/${tripId}/days`)
+    .then(resp => resp.json())
+    .catch(err => console.log(`ERROR: ${err}`))
+  }
+
+  static createDay(tripId) {
+    return fetch(`http://localhost:3000/trips/${tripId}/days`, {
+      method: 'POST'
+    })
+    .then(resp => resp.json())
+  }
+
+  static destroyDay(dayId) {
+    return fetch(`http://localhost:3000/days/${dayId}`, {
+      method: 'DELETE'
+    })
+    .then(resp => resp.json())
   }
 }
