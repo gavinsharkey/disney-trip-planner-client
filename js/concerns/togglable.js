1268ef9a23e135d782199f49fde35f9f90b261b4
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 13:18:08 -0400

    Remove conditional from toggleTripDiv method Add toggleDayReservations method

--- js/concerns/togglable.js
+++ js/concerns/togglable.js
@@ -2,12 +2,22 @@ class Togglable {
   static toggleTripDiv() {
     const div = document.querySelector('#new-trip-form')
     const tripDataDiv = document.querySelector('#trip')
-    if (tripDataDiv.classList.contains('hidden')) {
-      div.classList.add('hidden')
-      tripDataDiv.classList.remove('hidden')
+    div.classList.add('hidden')
+    tripDataDiv.classList.remove('hidden')
+  }
+
+  static toggleDayReservations(dayElement) {
+    const reseravtionsDiv = dayElement.querySelector('.reservation-list')
+    const liMessage = dayElement.querySelector('span.message')
+
+    if (reseravtionsDiv.classList.contains('hidden')) {
+      dayElement.classList.add('active')
+      reseravtionsDiv.classList.remove('hidden')
+      liMessage.innerText = 'Click to hide reservations'
     } else {
-      div.classList.remove('hidden')
-      tripDataDiv.classList.add('hidden')
+      dayElement.classList.remove('active')
+      reseravtionsDiv.classList.add('hidden')
+      liMessage.innerText = 'Click to view reservations'
     }
   }
 }
