c674b76ebcb544fa427ef6cee73279e7c1a13abb
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 09:13:17 -0400

    Add Reservation Class

---
+++ js/models/reservation.js
@@ -0,0 +1,27 @@
+class Reservation {
+  static all = []
+
+  constructor({ id, day_id, reservable_type, time, reservable: { name } }) {
+    this.id = id
+    this.dayId = day_id
+    this.type = reservable_type
+    this.time = time
+    this.name = name
+    Reservation.all.push(this)
+  }
+
+  static load(data) {
+    const res = new Reservation(data)
+    res.renderHTML()
+  }
+
+  renderHTML() {
+    const dayReservationList = document.querySelector(`li[data-day-id="${this.dayId}"] ul`)
+    dayReservationList.innerHTML += `
+      <li class="list-group-item list-group-item-dark p-1" data-reservation-id="${this.id}">
+        <span>${this.type}: ${this.name}</span>
+        <span>, at ${this.time}</span>
+      </li>
+    `
+  }
+}
