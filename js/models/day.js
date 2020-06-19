a3bd1f21a620d04e2e7461f8fe14931c6cf1f3ef
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 13:13:37 -0400

    Add clearDaysDiv static method to Day class Add loadDays static method to Day class Add loadMultiple static method to Day class Add destroy static method to Day class Add removeHTML method to Day class Add span with message to day li Wrap ul in div

--- js/models/day.js
+++ js/models/day.js
@@ -7,6 +7,25 @@ class Day {
     this.place = place
   }
 
+  static clearDaysDiv() {
+    const daysDiv = document.querySelector('#trip-days')
+    daysDiv.innerHTML = ""
+  }
+
+  static loadDays() {
+    const tripId = document.querySelector('#trip-data').dataset.tripId
+    this.clearDaysDiv()
+
+    API.loadDaysByTrip(tripId)
+    .then(json => this.loadMultiple(json))
+  }
+
+  static loadMultiple(days) {
+    for (let dayData of days) {
+      this.load(dayData)
+    }
+  }
+
   static load(data) {
     const day = new Day(data)
     day.renderHTML()
@@ -16,13 +35,26 @@ class Day {
     }
   }
 
+  static destroy(data) {
+    const day = new Day(data)
+    day.removeHTML()
+  }
+
+  removeHTML() {
+    const dayLi = document.querySelector(`li[data-day-id="${this.id}"]`)
+    dayLi.remove()
+  }
+
   renderHTML() {
     const daysDiv = document.querySelector('#trip-days')
     daysDiv.innerHTML += `
-      <li class="list-group-item list-group-item-primary p-1" data-day-id="${this.id}">
+      <li class="list-group-item list-group-item-primary p-2 day-data" data-day-id="${this.id}">
         <span>Day ${this.place}</span>
-        <button class="btn btn-dark btn-sm">Delete</button>
-        <ul class="list-group"></ul>
+        <button class="delete btn btn-dark btn-sm">Delete</button>
+        <span class="float-right text-muted message">Click to view reservations</span>
+        <div class="hidden reservation-list">
+          <ul class="list-group"></ul>
+        </div>
       </li>
     `
   }
