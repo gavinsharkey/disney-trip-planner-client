63dcb8a37fa7c6b46fbf8be397b905188a547252
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-03 09:26:07 -0400

    change api endpoint

--- js/models/api.js
+++ js/models/api.js
@@ -1,31 +1,31 @@
 class API {
   static loadResorts() {
-    return fetch('http://localhost:3000/resorts')
+    return fetch('https://disneytripapiproduction.herokuapp.com/resorts')
     .then(resp => resp.json())
   }
 
   static loadParks() {
-    return fetch('http://localhost:3000/parks')
+    return fetch('https://disneytripapiproduction.herokuapp.com/parks')
     .then(resp => resp.json())
   }
 
   static loadAttractionsByPark(parkId) {
-    return fetch(`http://localhost:3000/parks/${parkId}/attractions`)
+    return fetch(`https://disneytripapiproduction.herokuapp.com/parks/${parkId}/attractions`)
     .then(resp => resp.json())
   }
 
   static loadRestaurantsByPark(parkId) {
-    return fetch(`http://localhost:3000/parks/${parkId}/restaurants`)
+    return fetch(`https://disneytripapiproduction.herokuapp.com/parks/${parkId}/restaurants`)
     .then(resp => resp.json())
   }
 
   static loadRestaurantsByResort(resortId) {
-    return fetch(`http://localhost:3000/resorts/${resortId}/restaurants`)
+    return fetch(`https://disneytripapiproduction.herokuapp.com/resorts/${resortId}/restaurants`)
     .then(resp => resp.json())
   }
 
   static createTrip(tripName) {
-    return fetch('http://localhost:3000/trips', {
+    return fetch('https://disneytripapiproduction.herokuapp.com/trips', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -40,25 +40,25 @@ class API {
   }
 
   static getTripsData() {
-    return fetch('http://localhost:3000/trips')
+    return fetch('https://disneytripapiproduction.herokuapp.com/trips')
     .then(resp => resp.json())
     .catch(err => err)
   }
 
   static getTripData(tripId) {
-    return fetch(`http://localhost:3000/trips/${tripId}`)
+    return fetch(`https://disneytripapiproduction.herokuapp.com/trips/${tripId}`)
     .then(resp => resp.json())
     .catch(err => console.log(`ERROR: ${err}`))
   }
 
   static loadDaysByTrip(tripId) {
-    return fetch(`http://localhost:3000/trips/${tripId}/days`)
+    return fetch(`https://disneytripapiproduction.herokuapp.com/trips/${tripId}/days`)
     .then(resp => resp.json())
     .catch(err => console.log(`ERROR: ${err}`))
   }
 
   static updateTrip(tripId, tripName) {
-    return fetch(`http://localhost:3000/trips/${tripId}`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/trips/${tripId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json'
@@ -73,32 +73,32 @@ class API {
   }
 
   static destroyTrip(tripId) {
-    return fetch(`http://localhost:3000/trips/${tripId}`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/trips/${tripId}`, {
       method: 'DELETE'
     })
   }
 
   static createDay(tripId) {
-    return fetch(`http://localhost:3000/trips/${tripId}/days`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/trips/${tripId}/days`, {
       method: 'POST'
     })
     .then(resp => resp.json())
   }
 
   static destroyDay(dayId) {
-    return fetch(`http://localhost:3000/days/${dayId}`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/days/${dayId}`, {
       method: 'DELETE'
     })
     .then(resp => resp.json())
   }
 
   static getReservations(dayId) {
-    return fetch(`http://localhost:3000/days/${dayId}/reservations`)
+    return fetch(`https://disneytripapiproduction.herokuapp.com/days/${dayId}/reservations`)
     .then(resp => resp.json())
   }
 
   static createReservation({dayId, reservableId, reservableType, time}) {
-    return fetch(`http://localhost:3000/reservations`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/reservations`, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
@@ -116,7 +116,7 @@ class API {
   }
 
   static updateReservation(reservationId, newTime) {
-    return fetch(`http://localhost:3000/reservations/${reservationId}`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/reservations/${reservationId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json'
@@ -131,7 +131,7 @@ class API {
   }
 
   static destroyReservation(reservationId) {
-    return fetch(`http://localhost:3000/reservations/${reservationId}`, {
+    return fetch(`https://disneytripapiproduction.herokuapp.com/reservations/${reservationId}`, {
       method: 'DELETE'
     })
     .then(resp => resp.json())
